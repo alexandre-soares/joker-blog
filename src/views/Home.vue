e6d1d761014831cb93bdb1053b5bf8a7c4f47dce
Alexandre Soares <alexandresoares@Alexandres-MacBook-Pro.local>
2021-05-27 13:43:48 +0300

    adding register section

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -11,19 +11,33 @@
       <div class="container">
         <h3>View More Recent Blogs</h3>
         <div class="blog-cards">
-          <BlogCard v-for="(post, index) in sampleBlogCards" :key="index" :post="post" />
+          <BlogCard
+            v-for="(post, index) in sampleBlogCards"
+            :key="index"
+            :post="post"
+          />
         </div>
+      </div>
+    </div>
+
+    <div class="updates">
+      <div class="container">
+        <h2>Never miss a psot. Register for your free account today!</h2>
+        <router-link class="router-button" to="#"
+          >Register for Joker's Blog <Arrow class="arrow arrow-light"
+        /></router-link>
       </div>
     </div>
   </div>
 </template>
 
 <script>
+import Arrow from "@/assets/Icons/arrow-right-light.svg";
 import BlogCard from "../components/BlogCard.vue";
 import BlogPost from "../components/BlogPost.vue";
 export default {
   name: "Home",
-  components: { BlogPost, BlogCard },
+  components: { BlogPost, BlogCard, Arrow },
   data() {
     return {
       welcomeScreen: {
@@ -78,6 +92,44 @@ export default {
     font-weight: 300;
     font-size: 28px;
     margin-bottom: 32px;
+  }
+}
+
+.updates {
+  .container {
+    padding: 100px 25px;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+
+    @media (min-width: 800px) {
+      padding: 125px 25px;
+      flex-direction: row;
+    }
+
+    .router-button {
+      display: flex;
+      font-size: 14px;
+      text-decoration: none;
+
+      @media (min-width: 800px) {
+        margin-left: auto;
+      }
+    }
+
+    h2 {
+      font-weight: 300;
+      font-size: 32px;
+      max-width: 425px;
+      width: 100%;
+      text-align: center;
+      text-transform: uppercase;
+
+      @media (min-width: 800px) {
+        text-align: initial;
+        font-size: 40px;
+      }
+    }
   }
 }
 </style>
